e72f1be41d5795fd3fdd4103a8655b9a270bcf5d
jumgal <cumgal@gmail.com>
2022-08-01 12:41:18 -0500

    complete user registration

---
+++ support-frontend/src/features/auth/authService.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API_URL = "/api/users";
+
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  console.log("response  ", response);
+
+  return response.data;
+};
+
+// login user
+const login = async userData => {
+  const response = await axios.post(`${API_URL}/login`, userData)
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  console.log("response login ", response);
+
+  return response.data;
+}
+
+
+// logout
+const logout = () => localStorage.removeItem('user')
+
+const authService = {
+  register,
+  logout,
+  login
+};
+
+export default authService;
